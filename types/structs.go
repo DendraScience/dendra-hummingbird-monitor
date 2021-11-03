db450adb0430c990d9d645d6ac6b513c40e2b58f
Tai Groot <tai@taigrr.com>
2021-11-03 00:13:41 -0700

    add types

--- types/structs.go
+++ types/structs.go
@@ -9,29 +9,30 @@ import (
 )
 
 type QuarterHourly struct {
-	Timestamp time.Time `json:"timestamp"` // - Timestamp
-	Hostname  string    `json:"hostname"`  // - Hostname
-	Version   Version   `json:"version"`   // - Tickbird version
-
-	CollectionTime int64              `json:"collection_time"`   // - Time it takes for metrics collection
-	Containers     []docker.Container `json:"containers"`        // - Containers and statuses
-	DiskFree       int64              `json:"disk_free"`         // - Disk free
-	LANBytesDown   int64              `json:"lan_bytes_down"`    // - Lan Network bytes down
-	LANBytesUp     int64              `json:"lan_bytes_up"`      // - Lan Network bytes up
-	MemFree        int64              `json:"memory_free"`       // - Memory free
-	NumPackages    int                `json:"num_packages"`      // - Number of installed packages
-	UpdatesAvail   int                `json:"updates_available"` // - Package updates avail count
-	Uptime         int64              `json:"uptime"`            // - Uptime
-	WANBytesDown   int64              `json:"wan_bytes_down"`    // - Wan Network bytes down
-	WANBytesUp     int64              `json:"wan_bytes_up"`      // - Wan Network bytes up
-}
-
-type HNetworking struct {
-	LANBytesDown int64 `json:"lan_bytes_down"` // - Lan Network bytes down
-	LANBytesUp   int64 `json:"lan_bytes_up"`   // - Lan Network bytes up
-	WANBytesDown int64 `json:"wan_bytes_down"` // - Wan Network bytes down
-	WANBytesUp   int64 `json:"wan_bytes_up"`   // - Wan Network bytes up
-
+	CPU_Percent    float64            `json:"cpu_percent"`       // CPU Percentage usage
+	CollectionTime int64              `json:"collection_time"`   // Time it takes for metrics collection
+	Containers     []docker.Container `json:"containers"`        // Containers and statuses
+	DiskFree       int64              `json:"disk_free"`         // Disk free
+	DiskUsage      float64            `json:"disk_usage"`        // Disk usage
+	Hostname       string             `json:"hostname"`          // Hostname
+	LANBytesDown   int64              `json:"lan_bytes_down"`    // Lan Network bytes down
+	LANBytesUp     int64              `json:"lan_bytes_up"`      // Lan Network bytes up
+	LANIP          string             `json:"lan_ip"`            // IP of lan interface
+	LoadAverage    float64            `json:"load_average"`      // Load Average
+	MemAvail       int64              `json:"memory_avail"`      // Memory avail
+	MemBuffered    int64              `json:"memory_buffered"`   // Memory buffered
+	MemFree        int64              `json:"memory_free"`       // Memory free
+	MemPercent     float64            `json:"memory_percent"`    // Memory percentage
+	MemTotal       int64              `json:"memory_total"`      // Memory total
+	NumPackages    int                `json:"num_packages"`      // Number of installed packages
+	ProcessorCount int                `json:"processor_count"`   // Thread count
+	Timestamp      time.Time          `json:"timestamp"`         // Timestamp
+	UpdatesAvail   int                `json:"updates_available"` // Package updates avail count
+	Uptime         int64              `json:"uptime"`            // Uptime
+	Version        Version            `json:"version"`           // Hummingbird version
+	WANBytesDown   int64              `json:"wan_bytes_down"`    // Wan Network bytes down
+	WANBytesUp     int64              `json:"wan_bytes_up"`      // Wan Network bytes up
+	WANIP          string             `json:"wan_ip"`            // IP of wan interface
 }
 
 type Version struct {
