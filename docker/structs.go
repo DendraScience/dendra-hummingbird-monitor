e411fd011b4643534952695c24d9d68b0d8ed6dd
Tai Groot <tai@taigrr.com>
2021-11-03 00:14:29 -0700

    update usage to match types

--- docker/structs.go
+++ docker/structs.go
@@ -1,14 +1,15 @@
 package docker
 
+import "time"
+
 type Container struct {
-	ID         string  `json:"id"`
-	Name       string  `json:"name"`
-	CPU        float64 `json:"cpu"`
-	MemUsage   float64 `json:"mem_usage"`
-	MemAllowed float64 `json:"mem_allowed"`
-	MemPercent float64 `json:"mem_percent"`
-	Created    int64   `json:"time_created"`
-	Image      string  `json:"image"`
-	Tag        string  `json:"tag"`
-	Uptime     int64   `json:"uptime"`
+	ID         string    `json:"id"`
+	Name       string    `json:"name"`
+	CPU        float64   `json:"cpu"`
+	MemUsage   float64   `json:"mem_usage"`
+	MemAllowed float64   `json:"mem_allowed"`
+	MemPercent float64   `json:"mem_percent"`
+	Created    time.Time `json:"time_created"`
+	Image      string    `json:"image"`
+	Uptime     int64     `json:"uptime"`
 }
